ec86f65ac00499debac6ee248ce52758f28af50e
fredleluly <fredleluly@gmail.com>
2021-01-10 22:23:56 +0700

    memperbaiki bug body

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
-const showingmenu = (toggle,nav) => {
+const showingmenu = (toggle,nav,body) => {
     const toggleid = document.getElementById(toggle),
-    navbarid = document.getElementById(nav)
+    navbarid = document.getElementById(nav),
+    all = document.getElementById(body)
 
     if (toggleid && navbarid){
         toggleid.addEventListener('click',()=>{
             navbarid.classList.toggle('show')
+            all.classList.toggle('expand')
         })
     }
 }
-showingmenu('nav-toggle','navbar')
+showingmenu('nav-toggle','navbar','all')
